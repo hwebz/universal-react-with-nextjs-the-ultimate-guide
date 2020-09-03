edaa14c942aa13a97e1dd2ae026414e5035392d2
Do Manh Ha <ex-domanhha001@ocft.com>
2020-09-03 14:13:47 +0700

    Next links, serving static images

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
-const Index = () => <div>Welcome to the home page</div>;
+import Link from 'next/link';
+
+const Index = () => (
+    <div>
+        <h1>Home</h1>
+        <Link href="/about">
+            About
+        </Link>
+        <img src="/static/javascript-logo.png" alt="JavaScript" height="200px" />
+    </div>
+);
 
 export default Index;
