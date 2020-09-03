a6e68ca671a835c18839ef0944f325f62f5a87bc
Do Manh Ha <ex-domanhha001@ocft.com>
2020-09-03 15:02:10 +0700

    Fetching Data with getInitialProps

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,30 @@
+import React, { Component } from 'react';
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 import Layout from '../components/layout';
 
-const About = () => (
-    <Layout title="Hire Me">
-        <p>About page</p>
-    </Layout>
-);
+class About extends Component {
+    
+    static async getInitialProps() {
+        const res = await fetch('https://api.github.com/users/hwebz')
+        const data = await res.json();
+
+        return { user: data };
+    }
+
+    render() {
+        const { user } = this.props; 
+        return (
+            <Layout title="About">
+                <p>{user.name}</p>
+                <p>{user.bio}</p>
+                <img src={user.avatar_url}
+                    alt="JavaScript"
+                    height="200px"
+                />
+            </Layout>
+        );
+    }
+};
 
 export default About;
