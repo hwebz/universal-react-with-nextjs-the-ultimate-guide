ce11a3a2925c056e0f2463a867f4637b644efa80
Do Manh Ha <ex-domanhha001@ocft.com>
2020-09-03 17:31:27 +0700

    Fetching Next Page of Stories, Adding Pagination with Query Params

--- next-hackernews/pages/index.js
+++ next-hackernews/pages/index.js
@@ -1,25 +1,29 @@
 import fetch from 'isomorphic-unfetch';
 import Error from 'next/error';
+import Link from 'next/link';
 import Layout from '../components/Layout';
 import StoryList from '../components/StoryList';
 
 class Index extends React.Component {
 
-    static async getInitialProps() {
+    static async getInitialProps({ req, res, query }) {
         let stories;
+        let page;
+
         try {
-            const res = await fetch('https://node-hnapi.herokuapp.com/news?page=1');
+            page = Number(query.page) || 1;
+            const res = await fetch(`https://node-hnapi.herokuapp.com/news?page=${page}`);
             stories = await res.json();
         } catch (err) {
             console.log(err);
             stories = [];
         }
 
-        return { stories };
+        return { stories, page };
     }
 
     render() {
-        const { stories } = this.props;
+        const { stories, page } = this.props;
 
         if (stories.length === 0) {
             return <Error statusCode={503} />
@@ -28,6 +32,24 @@ class Index extends React.Component {
         return (
             <Layout title="Hacker Next" description="A Hacker News clone made with Next.js">
                 <StoryList stories={stories} />
+
+                <footer>
+                    <Link href={`/?page=${page + 1}`}>
+                        <a>Next page ({ page + 1 })</a>
+                    </Link>
+                </footer>
+
+                <style jsx>{`
+                    footer {
+                        padding: 1em;
+                    }
+
+                    footer a {
+                        font-weight: bold;
+                        color: black;
+                        text-decoration: none;
+                    }
+                `}</style>
             </Layout>
         )
     }
