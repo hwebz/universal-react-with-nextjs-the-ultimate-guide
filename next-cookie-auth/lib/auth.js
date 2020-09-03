a2b113be58dbbf4469b60c2b5d5c68a5ad6c9d01
Ha Manh Do <hadm.haui@gmail.com>
2020-09-03 23:47:38 +0700

    Persisting User Session with Custom Document Page

--- next-cookie-auth/lib/auth.js
+++ next-cookie-auth/lib/auth.js
@@ -3,13 +3,35 @@ import axios from 'axios';
 // Require cookie
 axios.defaults.withCredentials = true;
 
-export const loginUser = (email, password) => {
-    return axios.post('/api/login', {
+const WINDOW_USER_SCRIPT_VARIABLE = '__USER__';
+
+export const loginUser = async (email, password) => {
+    const { data } = await axios.post('/api/login', {
         email,
         password
     });
+    if (typeof window !== 'undefined') {
+        window[WINDOW_USER_SCRIPT_VARIABLE] = data || {};
+    }
 }
 
-export const getUserProfile = () => {
-    return axios.get('/api/profile');
+export const getUserProfile = async () => {
+    const { data } = await axios.get('/api/profile');
+    return data;
+}
+
+export const getServerSideToken = req => {
+    if (!req) return {};
+    
+    const { signedCookies = {}} = req;
+
+    if (!signedCookies || (signedCookies && !signedCookies.token)) {
+        return {};
+    } else {
+        return signedCookies;
+    }
+}
+
+export const getUserScript = user => {
+    return `${WINDOW_USER_SCRIPT_VARIABLE} = ${JSON.stringify(user)}`;
 }
