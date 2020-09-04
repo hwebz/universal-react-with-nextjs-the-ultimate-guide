9b505ac6352c5c521bc22c7bb54ccecf5c4526c9
Ha Manh Do <hadm.haui@gmail.com>
2020-09-04 23:15:38 +0700

    Basic Functionality for our  Signup Page

--- next-connect/components/Navbar.js
+++ next-connect/components/Navbar.js
@@ -4,44 +4,63 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import ShareOutlined from "@material-ui/icons/ShareOutlined";
 import withStyles from "@material-ui/core/styles/withStyles";
+import ActiveLink from "./ActiveLink";
 
 const Navbar = ({ classes, router, pageProps: { auth } }) => {
   const { user = {} } = auth || {};
   return (
-    <AppBar className={classes.appBar} position={router.pathname === "/" ? "fixed" : "static"}>
-      <Toolbar>
-        {/* Main Title / Home Button */}
-        <ShareOutlined className={classes.icon} />
-        <Typography variant="h5" component="h1" className={classes.toolbarTitle}>
-          NextConnect
-        </Typography>
-        {user._id ? (
-          // Auth Navigation
-          <div>
-            <Button>Profile</Button>
-            <Button variant="outlined">Sign out</Button>
-          </div>
-        ) : (
-          <div>
-            <Button>Sign in</Button>
-            <Button>Sign out</Button>
-          </div>
-        )}
-      </Toolbar>
-    </AppBar>
+	<AppBar className={classes.appBar} position={router.pathname === "/" ? "fixed" : "static"}>
+	  <Toolbar>
+		{/* Main Title / Home Button */}
+		<ActiveLink href="/">
+		  <ShareOutlined className={classes.icon} />
+		</ActiveLink>
+		<Typography variant="h5" component="h1" className={classes.toolbarTitle}>
+		  NextConnect
+		</Typography>
+		{user._id ? (
+		  // Auth Navigation
+		  <div>
+			<Button>
+			  <ActiveLink href="/profile">
+				Profile
+			  </ActiveLink>
+			</Button>
+			<Button variant="outlined">
+			  <ActiveLink href="/signout">
+				Sign out
+			  </ActiveLink>
+			</Button>
+		  </div>
+		) : (
+		  <div>
+			<Button>
+			  <ActiveLink href="/signin">
+				Sign in
+			  </ActiveLink>
+			</Button>
+			<Button>
+			  <ActiveLink href="signup">
+				Sign up
+			  </ActiveLink>
+			</Button>
+		  </div>
+		)}
+	  </Toolbar>
+	</AppBar>
   )
 };
 
 const styles = theme => ({
   appBar: {
-    // z-index 1 higher than the fixed drawer in home page to clip it under the navigation
-    zIndex: theme.zIndex.drawer + 1
+	// z-index 1 higher than the fixed drawer in home page to clip it under the navigation
+	zIndex: theme.zIndex.drawer + 1
   },
   toolbarTitle: {
-    flex: 1
+	flex: 1
   },
   icon: {
-    marginRight: theme.spacing.unit
+	marginRight: theme.spacing.unit
   }
 });
 
