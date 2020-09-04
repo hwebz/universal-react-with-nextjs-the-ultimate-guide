e1d7c4a528740b1aa90d51487cc3ee25458545ed
Ha Manh Do <hadm.haui@gmail.com>
2020-09-04 23:50:32 +0700

    Building Signing Form / Ability to Signing

--- next-connect/pages/signin.js
+++ next-connect/pages/signin.js
@@ -1,20 +1,116 @@
-// import Typography from "@material-ui/core/Typography";
-// import Avatar from "@material-ui/core/Avatar";
-// import FormControl from "@material-ui/core/FormControl";
-// import Paper from "@material-ui/core/Paper";
-// import Input from "@material-ui/core/Input";
-// import InputLabel from "@material-ui/core/InputLabel";
-// import Button from "@material-ui/core/Button";
-// import Snackbar from "@material-ui/core/Snackbar";
-// import Lock from "@material-ui/icons/Lock";
+import Router from "next/router";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import FormControl from "@material-ui/core/FormControl";
+import Paper from "@material-ui/core/Paper";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import Button from "@material-ui/core/Button";
+import Snackbar from "@material-ui/core/Snackbar";
+import Lock from "@material-ui/icons/Lock";
 import withStyles from "@material-ui/core/styles/withStyles";
+import { signinUser } from '../lib/auth';
 
 class Signin extends React.Component {
-  state = {};
+  state = {
+		email: '',
+		password: '',
+		openError: false,
+		error: '',
+		isLoading: false
+	};
 
-  render() {
-    return <div>Signin</div>;
-  }
+	handleChange = event => {
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	}
+
+	handleSubmit = event => {
+		const { email, password } = this.state;
+		event.preventDefault();
+		const user = {
+			email,
+			password
+		};
+
+		this.setState({
+			isLoading: true,
+			error: ''
+		})
+
+		signinUser(user)
+			.then(() => {
+				Router.push('/');
+			})
+			.catch(this.showError);
+	}
+
+	showError = err => {
+		const error = err.response && err.response.data || err.message;
+		this.setState({
+			openError: true,
+			error,
+			isLoading: false
+		})
+	}
+
+	handleClose = () => this.setState({ openError: false });
+
+	render() {
+		const { classes } = this.props;
+		const { error, openError, openSuccess, createdUser, isLoading } = this.state;
+
+		return <div className={classes.root}>
+			<Paper className={classes.paper}>
+				<Avatar className={classes.avatar}>
+				  <Lock />
+				</Avatar>
+				<Typography variant="h5" component="h1">Sign in</Typography>
+
+				<form onSubmit={this.handleSubmit} className={classes.form}>
+				<FormControl margin="normal" required fullWidth>
+					<InputLabel htmlFor="email">Email</InputLabel>
+					<Input
+					name="email"
+					type="email"
+					onChange={this.handleChange}
+					/>
+				</FormControl>
+				<FormControl margin="normal" required fullWidth>
+					<InputLabel htmlFor="password">Password</InputLabel>
+					<Input
+					name="password"
+					type="password"
+					onChange={this.handleChange}
+					/>
+				</FormControl>
+				<Button
+					type="submit"
+					fullWidth
+					variant="contained"
+					color="primary"
+					className={classes.submit}
+					disabled={isLoading}
+				>
+					{isLoading ? 'Signing in...' : 'Sign in'}
+				</Button>
+				</form>
+
+				{/* Error Snackbar */}
+				<Snackbar
+					anchorOrigin={{
+						vertical: 'bottom',
+						horizontal: 'right'
+					}}
+					open={openError}
+					onClose={this.handleClose}
+					autoHideDuration={4000}
+					message={<span className={classes.snack}>{error}</span>}
+				/>
+			</Paper>
+		</div>;
+	}
 }
 
 const styles = theme => ({
@@ -48,7 +144,7 @@ const styles = theme => ({
     marginTop: theme.spacing.unit * 2
   },
   snack: {
-    color: theme.palette.protectedTitle
+    color: theme.palette.secondary.light
   }
 });
 
